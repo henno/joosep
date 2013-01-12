e135ca62ef17bb7f1b3e5a7bc64cda66c831ccb5
jaakbusin <jaakbushin@gmail.com>
2013-01-12 12:45:05 +0200

    Added design (Twitter Bootstrap Starter project)

---
+++ application/classes/Controller/Login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lubuntu
+ * Date: 1/12/13
+ * Time: 12:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
--- application/logs/2013/01/12.php
+++ application/logs/2013/01/12.php
@@ -5,3 +5,25 @@
 #1 /var/www/joosep/system/classes/Kohana/Request.php(155): Kohana_Cookie::get('session')
 #2 /var/www/joosep/index.php(117): Kohana_Request::factory(true, Array, false)
 #3 {main} in /var/www/joosep/system/classes/Kohana/Cookie.php:67
+2013-01-12 04:14:59 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/joosep/system/classes/Kohana/View.php:137
+2013-01-12 04:14:59 --- DEBUG: #0 /var/www/joosep/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/joosep/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/joosep/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/joosep/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/joosep/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/joosep/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/joosep/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/joosep/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/joosep/system/classes/Kohana/View.php:137
+2013-01-12 04:16:22 --- EMERGENCY: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH/classes/Kohana/View.php [ 257 ] in /var/www/joosep/system/classes/Kohana/View.php:137
+2013-01-12 04:16:22 --- DEBUG: #0 /var/www/joosep/system/classes/Kohana/View.php(137): Kohana_View->set_filename('template')
+#1 /var/www/joosep/system/classes/Kohana/View.php(30): Kohana_View->__construct('template', NULL)
+#2 /var/www/joosep/system/classes/Kohana/Controller/Template.php(33): Kohana_View::factory('template')
+#3 /var/www/joosep/system/classes/Kohana/Controller.php(69): Kohana_Controller_Template->before()
+#4 [internal function]: Kohana_Controller->execute()
+#5 /var/www/joosep/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Welcome))
+#6 /var/www/joosep/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#7 /var/www/joosep/system/classes/Kohana/Request.php(990): Kohana_Request_Client->execute(Object(Request))
+#8 /var/www/joosep/index.php(118): Kohana_Request->execute()
+#9 {main} in /var/www/joosep/system/classes/Kohana/View.php:137
